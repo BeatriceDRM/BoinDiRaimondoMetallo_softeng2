fcb55b0cc860ef08fb7022d49904822638abb4f7
Beatrice <beatrice.diraimondo@mail.polimi.it>
2024-06-03 18:22:44 +0200

    changes13

--- job.sh
+++ job.sh
@@ -1,10 +1,5 @@
+
 #!/bin/bash
 
-# Load the required modules 
-module load singularity
-
-# Define the path to the Singularity image
-SINGULARITY_IMAGE=matr.sif
-
 # Run the application
-singularity exec matr.sif mpirun -n 2 /src/matrix_multiplication matrixA.txt matrixB.txt
+singularity exec matr.sif mpirun -n 2 ./main > output.txt  2> error.txt
