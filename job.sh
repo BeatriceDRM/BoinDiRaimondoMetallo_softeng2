9a685fb86c9333999d29d186600b0631700488d2
Beatrice <beatrice.diraimondo@mail.polimi.it>
2024-06-04 18:20:30 +0200

    changes21

--- job.sh
+++ job.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#SBATCH --job-name=my_job
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
 # Run the application
-singularity run matr.sif
+srun singularity run --bind $TMPDIR:/scratch_local matr.sif bash -c "mpirun -n 2 ./main"
